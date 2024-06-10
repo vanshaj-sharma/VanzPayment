2515649d3313dfd938f6c3d1bfc64d8583b8ea30
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-06-10 17:23:41 +0530

    updated db txns

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,13 +1,53 @@
 import express from "express";
-
+import db from "@repo/db/client";
 const app = express();
 
-app.post("/hdfcWebhook", (req, res) => {
+app.post("/hdfcWebhook", async (req, res) => {
   //TODO: Add zod validation here?
-  const paymentInformation = {
+  const paymentInformation: {
+    token: string;
+    userId: string;
+    amount: string;
+  } = {
     token: req.body.token,
-    userId: req.body.user_identifier,
+    userId: req.body.user_identitfier,
     amount: req.body.amount,
   };
-  // Update balance in db, add txn
+
+  try {
+    //updating money
+    await db.$transaction([
+      db.balance.updateMany({
+        where: {
+          userId: Number(paymentInformation.userId),
+        },
+        data: {
+          amount: {
+            increment: Number(paymentInformation.amount),
+          },
+        },
+      }),
+
+      //track the status of the txns, success or failed?
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+
+    res.json({
+      message: "Captured",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(411).json({
+      message: "Error while processing webhook",
+    });
+  }
 });
+
+app.listen(3003);
