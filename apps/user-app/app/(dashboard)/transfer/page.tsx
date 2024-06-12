5e2f18b0c1ca138fde64af21806965dbb5197a58
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-06-12 12:57:07 +0530

    created onramptransaction component

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,12 +1,14 @@
 import { AddMoneyCard } from "../../../components/AddMoneyCard";
 import { BalanceCard } from "../../../components/BalanceCard";
+import { OnRampTransaction } from "../../../components/OnRampTransaction";
 
 export default function () {
   return (
     <div>
       Hello from transfer
-      <AddMoneyCard />
-      <BalanceCard />
+      {/* <AddMoneyCard /> */}
+      {/* <BalanceCard /> */}
+      <OnRampTransaction />
     </div>
   );
 }
--- apps/user-app/components/OnRampTransaction.tsx
+++ apps/user-app/components/OnRampTransaction.tsx
@@ -1,3 +1,40 @@
-export const OnRampTransaction = () => {
-  return <div>hello from OnRampTransaction</div>;
+import { Card } from "@repo/ui/card";
+
+export const OnRampTransaction = ({
+  transactions,
+}: {
+  transactions: {
+    time: Date;
+    amount: number;
+    //TODO: find type of status
+    status: string;
+    provider: string;
+  }[];
+}) => {
+  if (!transactions) {
+    return (
+      <Card title={"Recent Transaction"}>
+        <div className="text-center pb-8 pt-8">No Recent Transactions</div>
+      </Card>
+    );
+  }
+  return (
+    <Card title={"Recent Transaction"}>
+      <div className="pt-2">
+        {transactions.map((t) => (
+          <div className="flex justify-between">
+            <div>
+              <div className="text-sm">Received INR</div>
+              <div className="text-slate-600 text-xs">
+                {t.time.toDateString()}
+              </div>
+              <div className="flex flex-col justify-center">
+                + Rs {t.amount / 100}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Card>
+  );
 };
