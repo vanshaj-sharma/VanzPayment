52804ca0ef0f0901e7dcaf3b63e6007be4a4c4ba
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-06-12 13:13:20 +0530

    completed transfer page

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,14 +1,60 @@
+import { getServerSession } from "next-auth";
 import { AddMoneyCard } from "../../../components/AddMoneyCard";
 import { BalanceCard } from "../../../components/BalanceCard";
 import { OnRampTransaction } from "../../../components/OnRampTransaction";
+import { authOptions } from "../../lib/auth";
+import prisma from "@repo/db/client";
 
-export default function () {
+//function to get balance
+const getBalance = async () => {
+  const session = await getServerSession(authOptions);
+  const balance = await prisma.balance.findFirst({
+    where: {
+      userId: session?.user?.id,
+    },
+  });
+
+  return {
+    amount: balance?.amount,
+    locked: balance?.locked,
+  };
+};
+
+//fxn to get txns
+const getOnRampTransactions = async () => {
+  const session = await getServerSession(authOptions);
+  const txns = await prisma.onRampTransaction.findMany({
+    where: {
+      userId: Number(session?.user?.id),
+    },
+  });
+  return txns.map((t) => ({
+    time: t.startTime,
+    amount: t.amount,
+    status: t.status,
+    provider: t.provider,
+  }));
+};
+
+export default async function () {
+  const balance = await getBalance();
+  const transactions = await getOnRampTransactions();
   return (
-    <div>
-      Hello from transfer
-      {/* <AddMoneyCard /> */}
-      {/* <BalanceCard /> */}
-      <OnRampTransaction />
+    <div className="w-screen">
+      <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+        Transfer
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+        <div>
+          <AddMoneyCard />
+        </div>
+        <div>
+          <BalanceCard amount={balance.amount} locked={balance.locked} />
+          <div className="pt-4">
+            <OnRampTransactions transactions={transactions} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
