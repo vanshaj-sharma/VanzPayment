d5cf0180f559c57c53c7c0844b9e19e9aaa4e30b
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-06-12 12:43:15 +0530

    completed balancecard component

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,10 +1,12 @@
 import { AddMoneyCard } from "../../../components/AddMoneyCard";
+import { BalanceCard } from "../../../components/BalanceCard";
 
 export default function () {
   return (
     <div>
       Hello from transfer
       <AddMoneyCard />
+      <BalanceCard />
     </div>
   );
 }
--- apps/user-app/components/BalanceCard.tsx
+++ apps/user-app/components/BalanceCard.tsx
@@ -1,3 +1,26 @@
-export const BalanceCard = () => {
-  return <div>hello from BalanceCard</div>;
+import { Card } from "@repo/ui/card";
+
+export const BalanceCard = ({
+  amount,
+  locked,
+}: {
+  amount: number;
+  locked: number;
+}) => {
+  return (
+    <Card title={"Balance"}>
+      <div className="flex justify-between border-b border-slate-300 pb-2">
+        <div>Unblocked Balance</div>
+        <div>{amount / 100} INR</div>
+      </div>
+      <div className="flex justify-between border-b border-slate-300 pb-2">
+        <div>Total Locked Balance</div>
+        <div>{locked / 100} INR</div>
+      </div>
+      <div className="flex justify-between border-b border-slate-300 pb-2">
+        <div>Total Balance</div>
+        <div>{(amount + locked) / 100} INR</div>
+      </div>
+    </Card>
+  );
 };
